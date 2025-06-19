ce11150a9ed37d14bc841bddf85997be7a94decf
jjjmmm876 <mauri_mar619@hotmail.com>
2025-06-19 17:14:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.set_page_config(page_title="FutbolIA Predict", layout="wide")
-st.image("logo.png", width=100)
+
 st.title("⚽ FutbolIA Predict")
 st.write("Bienvenido a tu app de predicciones de fútbol con IA.")
